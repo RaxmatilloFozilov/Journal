bb77c2d1fd32166538a428cbaea62e06e62e0810
RaxmatilloFozilov <152626671+RaxmatilloFozilov@users.noreply.github.com>
2024-05-20 01:08:27 +0500

    new
    new

--- app_journal/views.py
+++ app_journal/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
 
 
 from app_journal.models import (
@@ -21,6 +22,9 @@ class RequirementsViewSet(viewsets.ModelViewSet):
     queryset = Requirements.objects.all()
     serializer_class = RequirementsSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['title', 'description']
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
@@ -36,6 +40,9 @@ class FAQViewSet(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['question']
+    search_fields = ['question', 'answer']
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -51,6 +58,9 @@ class ContactsViewSet(viewsets.ModelViewSet):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
     permission_classes = [IsAdminUser]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name', 'message']
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
