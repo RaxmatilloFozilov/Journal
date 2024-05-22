2175e19e6f78b2028ef16e6fb7207ccb683095e0
RaxmatilloFozilov <152626671+RaxmatilloFozilov@users.noreply.github.com>
2024-05-22 19:02:23 +0500

    new
    new

--- app_journal/admin.py
+++ app_journal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-# from app_journal.models import
+from app_journal.models import Paper, Publication, JournalMain, PaperMain, Contacts
+
+admin.site.register(Paper)
+admin.site.register(Contacts)
+admin.site.register(JournalMain)
+admin.site.register(Publication)
+admin.site.register(PaperMain)
 # Register your models here.
